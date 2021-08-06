b45f67cab524508a21aa0cbbe3cfd1e8e6c58e89
AdrianoA <adriano.armendaris@gmail.com>
2021-08-06 10:50:18 -0500

    added questions to array following acceptance criteria

--- index.js
+++ index.js
@@ -4,7 +4,46 @@ const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: "Enter your project´s title",
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Write a brief description of your project',
+      },
+      {
+        type: 'input',
+        name: 'installation',
+        message: 'Describe your project´s installation instructions',
+        default: 'npm i',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Provide any relevant usage information about your project',
+      },
+      {
+        type: 'input',
+        name: 'contributing',
+        message: 'Provide any relevant contributing information about your project',
+      },
+      {
+        type: 'input',
+        name: 'test',
+        message: 'Provide instructions to run tests in your project',
+        default: 'npm test',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Please select a license template to add into your project',
+        choices: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+      },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
