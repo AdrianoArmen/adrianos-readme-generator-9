121f0dd45b30b02541cc52939204f17237895754
AdrianoA <adriano.armendaris@gmail.com>
2021-08-06 20:35:10 -0500

    license badge generatior function added using shields.io

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,14 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+
+// Using shields.io to render the license type banner taking the "license" param to display it dynamicly
+// https://shields.io/category/license
+function renderLicenseBadge(license) {
+  if (license !== 'None') {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`;
+  }
+  return '';
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
